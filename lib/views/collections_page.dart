46f58724739d3473dc57615e0c21fc91c8debf19
Ty694nite <taiyebahmed694@gmail.com>
2025-12-04 22:19:35 +0000

    Connect CollectionsViewModel to the CollectionsPage + show list of collections

--- lib/views/collections_page.dart
+++ lib/views/collections_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import '../view_models/collections_view_model.dart';
+import '../models/collection.dart';
 
 class CollectionsPage extends StatelessWidget {
   const CollectionsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Collections Page')),
+    final collections = CollectionsViewModel().getCollections();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Collections')),
+      body: ListView.builder(
+        itemCount: collections.length,
+        itemBuilder: (context, index) {
+          final Collection collection = collections[index];
+          return ListTile(
+            title: Text(collection.name),
+            onTap: () {
+              // You can add navigation to the collection page here later
+            },
+          );
+        },
+      ),
     );
   }
 }
