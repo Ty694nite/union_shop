4a0c493331b941a5599e231c18e8b144d16a2547
Ty694nite <taiyebahmed694@gmail.com>
2025-12-04 18:53:35 +0000

    Add reusable widgets to product_card.dart

---
+++ lib/views/product_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String title;
+  final String price;
+  final String imageUrl;
+
+  const ProductCard({
+    super.key,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(imageUrl),
+          Text(title),
+          Text(price),
+        ],
+      ),
+    );
+  }
+}
