1894d7e6a8d815ff621601c2447b125d5023fd85
Ty694nite <taiyebahmed694@gmail.com>
2025-12-05 08:32:24 +0000

    Changing order of product card to see if price shows on homepage

--- lib/views/product_card.dart
+++ lib/views/product_card.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
+  final String imageUrl;
   final String title;
   final String price;
-  final String imageUrl;
 
   const ProductCard({
     super.key,
+    required this.imageUrl,
     required this.title,
     required this.price,
-    required this.imageUrl,
   });
 
   @override
