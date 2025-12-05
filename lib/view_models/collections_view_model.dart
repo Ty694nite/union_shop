d69a359fb6604abc44be0b5aa15d908bb8b73c0f
Ty694nite <taiyebahmed694@gmail.com>
2025-12-05 10:19:38 +0000

    Update collections view model

--- lib/view_models/collections_view_model.dart
+++ lib/view_models/collections_view_model.dart
@@ -3,8 +3,22 @@ import '../models/collection.dart';
 class CollectionsViewModel {
   List<Collection> getCollections() {
     return [
-      Collection(id: '1', name: 'Clothing'),
-      Collection(id: '2', name: 'Accessories'),
+      Collection(
+        id: '1',
+        name: 'Clothing',
+        imageUrl: 'assets/images/green_t_shirt.WEBP',
+      ),
+      Collection(
+        id: '2',
+        name: 'Accessories',
+        imageUrl: 'assets/images/magnet_1.JPEG',
+      ),
+      Collection(
+        id: '3',
+        name: 'Signature',
+        imageUrl: 'assets/images/signature_hoodie.WEBP',
+      ),
+      // Add more collections as needed
     ];
   }
 }
