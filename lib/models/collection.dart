7f6277f84a8684e02f770c3245ab349f696e89e0
Ty694nite <taiyebahmed694@gmail.com>
2025-12-05 10:18:22 +0000

    Update collection model

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,6 +1,7 @@
 class Collection {
   final String id;
   final String name;
+  final String imageUrl;
 
-  Collection({required this.id, required this.name});
+  Collection({required this.id, required this.name, required this.imageUrl});
 }
