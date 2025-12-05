6767b4075d3cf8debcaa30efc245d11827a750d9
Ty694nite <taiyebahmed694@gmail.com>
2025-12-05 08:50:45 +0000

    Add singature product list to home_view_model.dart

--- lib/view_models/home_view_model.dart
+++ lib/view_models/home_view_model.dart
@@ -10,9 +10,24 @@ class HomeViewModel {
           imageUrl: 'images/pink_essential_hoodie.WEBP'),
       Product(
           id: '2',
-          title: 'Essential T Shirt',
+          title: 'Essential T-Shirt',
           price: '£6.99',
           imageUrl: 'images/green_t_shirt.WEBP'),
+    ];
+  }
+
+  List<Product> getSignatureProducts() {
+    return [
+      Product(
+          id: '3',
+          title: 'Signature Hoodie',
+          price: '£29.99',
+          imageUrl: 'images/signature_hoodie.WEBP'),
+      Product(
+          id: '4',
+          title: 'Signature T_Shirt',
+          price: '£9.99',
+          imageUrl: 'images/signature_t_shirt.WEBP'),
     ];
   }
 }
